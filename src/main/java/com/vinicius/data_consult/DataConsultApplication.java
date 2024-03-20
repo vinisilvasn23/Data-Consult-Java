b4b0d3ff0aacaac449d8d2cfdf23afa435b643d4
Vinicius <vini456nascimento@gmail.com>
2024-03-20 15:20:38 -0300

    init app

---
+++ src/main/java/com/vinicius/data_consult/DataConsultApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.data_consult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataConsultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/data_consult/DataConsultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.data_consult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
